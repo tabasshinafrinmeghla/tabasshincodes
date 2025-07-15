b7d35d32cb6fe532a3a934afc64195f30cbd7eab
TabasshinAfrinMeghla <tabasshinafrinmeghla@gmail.com>
2025-07-15 22:44:54 +0600

    add all section

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,25 @@
+import Approach from "@/components/Approach";
+import Clients from "@/components/Clients";
+import Experience from "@/components/Experience";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
+import RecentProject from "@/components/RecentProject";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
-import { GoHome } from "react-icons/go";
+import { navItems } from "@/data";
+
 
 
 export default function Home() {
   return (
-    <main className="relative bg-black flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-16 ">
+    <main className="relative bg-black flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-16">
       <div className="max-w-7xl w-full">
-        <FloatingNav navItems={[{
-          name: 'Hone', link: '/', icon: <GoHome />
-        }]} />
+        <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
+        <RecentProject />
+        <Clients />
+        <Experience />
+        <Approach />
       </div>
     </main>
 
