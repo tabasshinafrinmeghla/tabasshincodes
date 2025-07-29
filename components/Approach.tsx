3bcfaeee87a50767dbb6228fe17fcd467cff099b
TabasshinAfrinMeghla <tabasshinafrinmeghla@gmail.com>
2025-07-29 22:53:09 +0600

    Fix: type error in Icon component

--- components/Approach.tsx
+++ components/Approach.tsx
@@ -122,7 +122,7 @@ const AceternityIcon = ({ order }: { order: string }) => {
   );
 };
 
-export const Icon = ({ className, ...rest }: any) => {
+export const Icon = ({ className, ...rest }: React.SVGProps<SVGSVGElement>) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -137,5 +137,6 @@ export const Icon = ({ className, ...rest }: any) => {
     </svg>
   );
 };
+
 
 export default Approach;
